fc0bd8e3e91ba282bfe7a6e068109801c1e00f92
AnandaCahya <tgigroupjap@gmail.com>
2025-05-24 08:42:14 +0700

    feat [Dev Mode] Add delay restart after file change

--- core/runner.js
+++ core/runner.js
@@ -7,6 +7,8 @@ const { spawnProcess, restartProcess, generateShardName } = require('./spawner')
 async function runShards(config, argv) {
     const shardCount = config?.shardScheme?.total ?? 1;
 
+    let waiting;
+
     if (typeof argv.shardId === "number") {
         const shardName = generateShardName(argv.shardId, config.name);
         spawnProcess(argv.shardId, shardName, config, argv, dir);
@@ -17,8 +19,10 @@ async function runShards(config, argv) {
                 ignoreInitial: true,
                 persistent: true
             }).on('all', () => {
-                console.log(chalk.yellow(`[DEV] Detected change, restarting shard ${argv.shardId}...`));
-                restartProcess(argv.shardId, shardName, config, argv, dir);
+                waiting = setTimeout(function () {
+                    console.log(chalk.yellow(`[DEV] Detected change, restarting shard ${argv.shardId}...`));
+                    restartProcess(argv.shardId, shardName, config, argv, dir);
+                }, 1000)
             });
         }
 
@@ -34,11 +38,14 @@ async function runShards(config, argv) {
                 ignored: ignoreFile,
                 ignoreInitial: true
             }).on('all', () => {
-                console.log(chalk.yellow(`[DEV] Detected change, restarting all shards...`));
-                for (let i = 0; i < shardCount; i++) {
-                    const shardName = generateShardName(i, config.name);
-                    restartProcess(i, shardName, config, argv, dir);
-                }
+                waiting = setTimeout(async function () {
+                    console.log(chalk.yellow(`[DEV] Detected change, restarting all shards...`));
+                    for (let i = 0; i < shardCount; i++) {
+                        const shardName = generateShardName(i, config.name);
+                        restartProcess(i, shardName, config, argv, dir);
+                        await delay(config?.shardScheme?.delay ?? 2000);
+                    }
+                }, 1000)
             });
         }
     }
